870aa9db5aae252ae0e284ef17f7654fff02d739
José Furtado <josefurtado99@outlook.com>
2018-07-31 21:58:28 -0300

    #ADD EntidadeBase

---
+++ Gerasite.Dominio/Common/EntidadeBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerasite.Dominio.Common
+{
+    public class EntidadeBase
+    {
+        public int Id { get; set; }
+        public bool Deletado { get; set; }
+    }
+}
