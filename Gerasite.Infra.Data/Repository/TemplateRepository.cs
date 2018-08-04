9beb26c65a8501360dd78beb7d2758419231ae93
GerasiteProject <41810253+GerasiteProject@users.noreply.github.com>
2018-08-03 23:27:35 -0300

    #add Template e TemplateArquivado
    commitando Template e TemplateArquivado

---
+++ Gerasite.Infra.Data/Repository/TemplateRepository.cs
@@ -0,0 +1,22 @@
+﻿using Gerasite.Dominio.Entidades;
+using Gerasite.Dominio.Repository;
+using Gerasite.Infra.Data.Context;
+using Gerasite.Infra.Data.Repositorio;
+using System;
+
+namespace Gerasite.Infra.Data.Repository
+{
+    class TemplateRepository : Repository<Template>, ITemplateRepository
+    {
+        public TemplateRepository(GerasiteContext context)
+            : base(context)
+        {
+
+        }
+
+        public Template GetByNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gerasite.Negocio/Services/TemplateArquivadoService.cs
@@ -0,0 +1,54 @@
+﻿using Gerasite.Dominio.Entidades;
+using Gerasite.Dominio.Services;
+using Gerasite.Infra.Data.Transaction;
+using System.Collections.Generic;
+
+namespace Gerasite.Negocio.Services
+{
+    public class TemplateArquivadoService : ITemplateArquivadoService
+    {
+        private readonly IUnityOfWork _Uow;
+
+        public TemplateArquivadoService(IUnityOfWork Uow)
+        {
+            this._Uow = Uow;
+        }
+
+        public void Delete(int id)
+        {
+            _Uow.GetRepository<TemplateArquivado>().Remove(id);
+        }
+
+        public IEnumerable<TemplateArquivado> Get()
+        {
+            return _Uow.GetRepository<TemplateArquivado>().GetAll();
+        }
+
+        public TemplateArquivado Get(int id)
+        {
+            return _Uow.GetRepository<TemplateArquivado>().GetById(id);
+        }
+
+        public void SaveOrUpdate(TemplateArquivado entity)
+        {
+            if (entity.Id == 0)
+            {
+                _Uow.GetRepository<TemplateArquivado>().Add(entity);
+                _Uow.GetRepository<TemplateArquivado>().SaveChanges();
+            }
+            else
+            {
+                _Uow.GetRepository<TemplateArquivado>().Update(entity);
+
+            }
+        }
+
+        public void Dispose()
+        {
+            _Uow.GetRepository<TemplateArquivado>().Dispose();
+        }
+
+
+    }
+}
+
