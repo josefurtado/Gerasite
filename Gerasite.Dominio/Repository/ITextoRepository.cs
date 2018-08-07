086c874e64d26faae0d3135e8e1137741c9e6069
José Furtado <josefurtado99@outlook.com>
2018-08-06 21:07:38 -0300

    Revert "Revert "Revert "Merge branch 'branch_Themystocles'"""
    This reverts commit a43e4160235f77d48223e24b95f42b028711db8b.

--- Gerasite.Dominio/Repository/ITextoRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Gerasite.Dominio.Entidades;
-
-namespace Gerasite.Dominio.Interfaces
-{
-    public interface ITextoRepository : IRepository<Texto>
-    {
-        Texto GetByNome(string nome);
-    }
-}
