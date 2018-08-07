3d5fd23d5325973b568c9a5e75fa5fb072b716a1
José Furtado <josefurtado99@outlook.com>
2018-08-06 21:01:28 -0300

    Revert "Merge branch 'branch_Themystocles'"
    This reverts commit 306a567cd89d1775197a2d7d55da16abdd27eed2, reversing
    changes made to 3b863fd8a50565b1526515509b18a85df7b06204.

--- Gerasite.Dominio/Entidades/Texto.cs
+++ Gerasite.Dominio/Entidades/Texto.cs
@@ -7,6 +7,5 @@
         public string CorTexto { get; set; }
         public float PosicaoHorizontal { get; set; }
         public float PosicaoVertical { get; set; }
-        public string Nome { get; set; }
     }
 }
