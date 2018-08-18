21397c776f23256bebf886290ad35c4725202eae
Everton <evsantos96@gmail.com>
2018-08-18 18:44:29 -0300

    #Update Sidebar
    Correção de erros no layout da view do painel administrativo do usuario

---
+++ Gerasite.Web/Scripts/dashboard.js
@@ -0,0 +1,19 @@
+﻿
+$(document).ready(function () {
+    $(".zoom").hover(function () {
+        $(".edit").fadeIn(1000);
+    }, function () {
+        $(".edit").fadeOut(800);
+    }
+    );
+});
+
+$(document).ready(function () {
+    $(".zoom1").hover(function () {
+        $(".edit1").fadeIn(1000);
+    }, function () {
+        $(".edit1").fadeOut(800);
+    }
+    );
+});
+
