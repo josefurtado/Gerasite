19181104f5e0e36301cb3a633a344c504e1d06b1
Everton <evsantos96@gmail.com>
2018-08-20 22:38:59 -0300

    #remove

--- Gerasite.Web/Controllers/UsuarioController.cs
+++ Gerasite.Web/Controllers/UsuarioController.cs
@@ -1,7 +1,5 @@
 ﻿using Gerasite.Dominio.Entidades;
 using Gerasite.Dominio.Services;
-using Gerasite.Infra.Data.Context;
-using System.Linq;
 using System.Web.Mvc;
 
 namespace Gerasite.Web.Controllers
@@ -17,16 +15,9 @@ namespace Gerasite.Web.Controllers
 
         public ActionResult Index()
         {
-            if (Session["Id"] != null)
-            {
-                return View();
-            }
-            else
-            {
-                return RedirectToAction("Login");
-            }
+            return View();
         }
-
+      
         public ActionResult CadastrarUsuario()
         {
             return View();
@@ -39,7 +30,7 @@ namespace Gerasite.Web.Controllers
             {
                 if (ModelState.IsValid)
                 {
-                    _service.SaveOrUpdate(usuario);
+                    _service.SaveOrUpdate(usuario);                   
                     return RedirectToAction("Index");
                 }
                 return View(usuario);
@@ -48,63 +39,6 @@ namespace Gerasite.Web.Controllers
             {
                 return View(usuario);
             }
-        }
-
-        public ActionResult EditarUsuario(int id)
-        {
-            Usuario usuario = _service.Get(id);
-            if (usuario == null)
-            {
-                return HttpNotFound();
-            }
-            return View(usuario);
-        }
-
-        [HttpPost]
-        public ActionResult EditarUsuario(Usuario usuario)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    _service.SaveOrUpdate(usuario);
-                    return RedirectToAction("Index", "Usuario");
-                }
-                return View(usuario);
-            }
-            catch
-            {
-                return View(usuario);
-            }
-        }
-
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(Usuario user)
-        {
-            if (ModelState.IsValid)
-            {
-                using (GerasiteContext varBanco = new GerasiteContext())
-                {
-                    var usuario = varBanco.Usuarios.Where(u => u.Nome.Equals(user.Nome) && u.Senha.Equals(user.Senha)).FirstOrDefault();
-                    if (usuario != null)
-                    {
-                        Session["Nome"] = usuario.Nome;
-                        Session["Id"] = usuario.Id;
-                        return RedirectToAction("Index", "Usuario");
-                    }
-                    if (usuario == null)
-                    {
-                        Session["Message"] = "Dados Incorretos!";
-                    }
-                }
-            }
-            return View(user);
         }
     }
 }
