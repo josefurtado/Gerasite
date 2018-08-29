04acb8f43ce8d37129df1c890c0ad7e625ac6e43
KatarinaGaldino <katarinagaldino24@gmail.com>
2018-08-29 17:05:43 -0300

    #Update Camada de Aplicação

---
+++ Gerasite.Application/Configuration/ApplicationSignInManager.cs
@@ -0,0 +1,9 @@
+﻿using Gerasite.Application.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace Gerasite.Application.Configuration
+{
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+    }
+}
