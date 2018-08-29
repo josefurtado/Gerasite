9b97c390693836dd726ba2c5fae1465843219c26
KatarinaGaldino <katarinagaldino24@gmail.com>
2018-08-29 16:14:38 -0300

    #Update Camada de Aplicação

---
+++ Gerasite.Web/App_Start/Startup.Auth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using EP.IdentityIsolation.Infra.CrossCutting.Identity.Configuration;
+using EP.IdentityIsolation.Infra.CrossCutting.Identity.Model;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
+
+namespace EP.IdentityIsolation.MVC
+{
+    public partial class Startup
+    {
+       
+    }
+}
