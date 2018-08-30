a04ab8bb2194fb80c3ebb29a0bc3622f5a21da47
Everton <evsantos96@gmail.com>
2018-08-30 15:14:55 -0300

    Revert "Merge branch 'Branch_Katarina' into Branch_EvertonSantos"
    This reverts commit 240cf3ed2b99cbaad13bac4871461f39d48860fd, reversing
    changes made to adff17ebb48c1583a31931bee9eb1860b7babdf9.

--- Gerasite.Web/Controllers/AccountController.cs
+++ Gerasite.Web/Controllers/AccountController.cs
@@ -1,61 +1,17 @@
-﻿using Gerasite.Application.Models;
-using Gerasite.Application.ViewModels.AccountViewModels;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Gerasite.Application;
 
 namespace Gerasite.Web.Controllers
 {
     public class AccountController : Controller
     {
-        private GerenciadorUsuario GerenciadorUsuario
-        {
-            get
-            {
-                return HttpContext.GetOwinContext().GetUserManager<GerenciadorUsuario>();
-            }
-        }
-
-        public ActionResult Register()
+        // GET: Account
+        public ActionResult Login()
         {
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                UsuarioIdentity user = new UsuarioIdentity { UserName = model.Nome, Email = model.Email };
-                if (model.Senha == model.ConfirmaSenha)
-                {
-                    IdentityResult result = GerenciadorUsuario.Create(user, model.Senha);
-                    if (result.Succeeded)
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                    else
-                    {
-                        AddErrorsFromResult(result);
-                    }
-                }
-
-                else
-                {
-                    ModelState.AddModelError("", "Senhas não correspondem.");
-                }
-            }
-            return View(model);
-        }
-       
-        private void AddErrorsFromResult(IdentityResult result)
-        {
-            foreach (string error in result.Errors)
-            {
-                ModelState.AddModelError("", error);
-            }
         }
     }
 }
